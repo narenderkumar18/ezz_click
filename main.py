e4ad51e0f14df87994ae6be4b667036a7868842a
Narender <narender78966.com>
2022-02-14 17:54:44 +0530

    Added stop botton using multithreading

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+from statistics import quantiles
+from threading import Thread
 import time
 import mouse
 import pyautogui
+import keyboard
 
 first_MousePos = mouse.get_position()
 second_MousePos = mouse.get_position()
-while True:
+shouldStop = False
+ 
+def threaded_function():
+    global shouldStop
+    while True:
+       if keyboard.read_key() == "x":
+            shouldStop = True
+            break
+
+thread = Thread(target = threaded_function)
+thread.start()
+
+while not shouldStop:
     first_MousePos = mouse.get_position()
     time.sleep(1)
     second_MousePos = mouse.get_position()
     if second_MousePos == first_MousePos:
         mouse.click('left')
-        pyautogui.press('Tab')
-        pyautogui.keyUp('Tab')
+        pyautogui.press('q')
+        pyautogui.keyUp('q')
+
+print('programm stopped')
